b3184108f5c637a2ed0af6fa201b718f53f40c36
Jason Leaman <jason.leaman.1982@gmail.com>
2021-02-01 11:38:07 +0000

    Moving terraform code (#6)
    * Moving terraform code

---
+++ terraform/backend/development.tfvars
@@ -0,0 +1,4 @@
+resource_group_name  = "rg-azure-githubactions"
+storage_account_name = "saazuregithubactions"
+container_name       = "azure-githubactions"
+key                  = "dev.terraform.tfstate"
---
+++ terraform/main.tf
@@ -0,0 +1,28 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "example-aks1"
+  location            = "North Europe"
+  resource_group_name = "rg-azure-githubactions"
+  dns_prefix          = "exampleaks1"
+  default_node_pool {
+    name                 = "default"
+    node_count           = 1
+    vm_size              = "Standard_B2S"
+    orchestrator_version = "1.18.14"
+  }
+  kubernetes_version = "1.18.14"
+  identity {
+    type = "SystemAssigned"
+  }
+  tags = {
+    Environment = var.environment
+  }
+}
+
+resource "local_file" "kubeconfig" {
+  content  = azurerm_kubernetes_cluster.example.kube_config_raw
+  filename = pathexpand("~/.kube/config")
+}
